0ea05671f492aab631a03b5a2c393663ac997587
David Emery <Emery2100@yahoo.com>
2026-03-24 17:07:06 -0700

    Initial Commit

---
+++ WeatherAppClass1/WeatherAppClass1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppClass1App.swift
+//  WeatherAppClass1
+//
+//  Created by David Emery on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppClass1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
